d0c1f66258990087d612e9864e8ae67dab3378cc
+ <jacqueline.trapp18@gmail.com>
2024-12-09 15:04:20 -0500

    specifying host for cors

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const patchRoutes = require('./patch');
 const app = express();
 const port = process.env.MYSQLPORT || 8080;
 
-app.use(cors());
+// Enable CORS for localhost:3000 only
+app.use(cors({
+    origin: 'http://localhost:3000', // Allow only this origin
+  }));
 app.use(express.json()); // Parse JSON in request body
 
 // Mount routes here
