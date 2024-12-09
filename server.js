d77a8833779969c8b9fae58f580a340397d85262
+ <jacqueline.trapp18@gmail.com>
2024-12-09 12:23:53 -0500

    fix bug duplicate declaration of express

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
-
-const express = require('express');
 const cors = require('cors'); // Import the cors package
 
 const app = express();
